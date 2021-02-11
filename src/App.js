ceb570905c058b5d2ccad64197b4e45c323e9bee
Beardificent <cis.magito@gmail.com>
2021-02-11 12:20:03 +0100

    initial commit for github, created a client to fetch from spring boot, created a table to receive data with antDesign framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+import {getAllStudents} from './client'
 import './App.css';
+import React, {Component} from "react";
+//antd is a UI framework
+import { Table, Avatar, Spin } from "antd";
+import { LoadingOutlined } from '@ant-design/icons';
+import Container from './Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getIndicatorIcon = () => {
+    return <LoadingOutlined style={{ fontSize: 24 }} spin/>;
+}
+class App extends Component {
+
+    //state contains an object which is an emty array called students.
+    state = {
+        students: [],
+        isFetching: false
+    }
+
+    componentDidMount() {
+        this.fetchStudents();
+
+    }
+
+    fetchStudents = () => {
+         this.setState({ isFetching: true });
+        getAllStudents().then(response => response.json().then(students => {
+            console.log(students);
+            this.setState({students, isFetching: false});
+        }));
+    }
+
+    render() {
+
+
+        //set object with const and this.state (state can be found above)
+        const {students, isFetching} = this.state;
+
+        if (isFetching) {
+            return (
+                <Container>
+                    <Spin indicator={getIndicatorIcon()}/>
+                </Container>
+            );
+        }
+        if (students && students.length) {
+
+            const columns = [
+                {
+                    title: '',
+                    key: 'avatar',
+                    //render takes text and studentobject
+                    render: (text, student) => (
+                        <Avatar size="large">{`${student.firstName.charAt(0).toUpperCase()}${student.lastName.charAt(0).toUpperCase()}`}</Avatar>
+                    )
+                },
+                {
+                    title: 'student ID',
+                    dataIndex: 'studentId',
+                    key: 'studentId',
+                },
+                {
+                    title: 'First Name',
+                    dataIndex: 'firstName',
+                    key: 'firstName',
+                },
+                {
+                    title: 'Last Name',
+                    dataIndex: 'lastName',
+                    key: 'lastName',
+                },
+                {
+                    title: 'Email',
+                    dataIndex: 'email',
+                    key: 'email',
+                },
+                {
+                    title: 'Gender',
+                    dataIndex: 'gender',
+                    key: 'gender',
+                }
+            ];
+
+            return (
+                <Container>
+                    <Table dataSource={students}
+                           columns={columns} pagination={false}
+                           rowKey="studentId"/>
+                </Container>
+            );
+
+        }
+        return <h1>No students</h1>
+
+    }
 }
 
 export default App;
